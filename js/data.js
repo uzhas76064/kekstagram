81cda2794cffdc5d1db5846808db70ac2035cb07
Maksim Hvatov <uzhas76064@gmail.com>
2023-08-22 21:15:41 +0300

    added function generating unique id from min value to max value

--- js/data.js
+++ js/data.js
@@ -1,6 +1,5 @@
 import {getRandomArray, getRandomPositiveInteger} from "./utils.js";
 
-// Генерация объекта фото
 const MIN_VALUE = 1;
 const MAX_VALUE = 25;
 const COMMENTS = ['Всё отлично!',
@@ -10,19 +9,38 @@ const COMMENTS = ['Всё отлично!',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 const NAMES = ['Данил', 'Андрей', 'Себастьян', 'Паула', 'Георг'];
+const PHOTOS_COUNT = 25;
 
+// Генерация не повторяющихся ID с помощью замыкания
+const createRandomIdFromGenerator = (min, max) => {
+  const previousValues = [];
 
+  return function () {
+    let currentValue = getRandomPositiveInteger(min, max);
+
+    if (previousValues.length >= (max - min + 1)) {
+      console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
+      return null;
+    }
+
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomPositiveInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  }
+}
 
 const generatePhotosObject = () => {
   return {
-    id: getRandomPositiveInteger(MIN_VALUE, MAX_VALUE),
-    url: `photos/${getRandomPositiveInteger(MIN_VALUE, MAX_VALUE)}.jpg`,
+    id: createRandomIdFromGenerator(MIN_VALUE, MAX_VALUE),
+    url: `photos/${createRandomIdFromGenerator(MIN_VALUE, MAX_VALUE)}.jpg`,
     likes: getRandomPositiveInteger(15, 200),
 
     comments: [
       {
-        id: getRandomPositiveInteger(1, 150),
-        avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
+        id: createRandomIdFromGenerator(1, 150),
+        avatar: `img/avatar-${createRandomIdFromGenerator(1, 6)}.svg`,
         message: getRandomArray(COMMENTS),
         name: getRandomArray(NAMES)
       }
@@ -30,6 +48,6 @@ const generatePhotosObject = () => {
   }
 }
 
-const createPhotos = () => Array.from({length: 25}, generatePhotosObject);
+const createPhotos = () => Array.from({length: PHOTOS_COUNT}, generatePhotosObject);
 
 export {createPhotos};
