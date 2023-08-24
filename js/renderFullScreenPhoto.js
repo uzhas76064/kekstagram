296b889b0b1b80239ff553114e3a96825182c64e
Maksim Hvatov <uzhas76064@gmail.com>
2023-08-24 12:39:54 +0300

    added functions closing big picture modal

--- js/renderFullScreenPhoto.js
+++ js/renderFullScreenPhoto.js
@@ -1,5 +1,24 @@
 const bigPicture = document.querySelector('.big-picture');
 const pictures = document.querySelectorAll('.picture');
+const pictureClose = bigPicture.querySelector('.cancel');
+
+const hideBigPicture = () => {
+  bigPicture.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+};
+
+document.addEventListener('keydown', (e) => {
+  e.preventDefault();
+
+  if (e.key === 'Escape') {
+    hideBigPicture();
+  }
+});
+
+pictureClose.addEventListener('click', (e) => {
+  e.preventDefault();
+  hideBigPicture();
+});
 
 pictures.forEach((picture) => {
   picture.addEventListener('click', () => {
@@ -7,3 +26,5 @@ pictures.forEach((picture) => {
     document.body.classList.add('modal-open');
   });
 });
+
+
