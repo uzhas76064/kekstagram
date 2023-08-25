0c2a81ae36e5d37952b0df895d66193395ecacee
Maksim Hvatov <uzhas76064@gmail.com>
2023-08-25 12:40:32 +0300

    added big picture rendering

--- js/renderFullScreenPhoto.js
+++ js/renderFullScreenPhoto.js
@@ -1,6 +1,12 @@
+
 const bigPicture = document.querySelector('.big-picture');
 const pictures = document.querySelectorAll('.picture');
 const pictureClose = bigPicture.querySelector('.cancel');
+
+const showBigPicture = () => {
+  bigPicture.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+};
 
 const hideBigPicture = () => {
   bigPicture.classList.add('hidden');
@@ -22,8 +28,15 @@ pictureClose.addEventListener('click', (e) => {
 
 pictures.forEach((picture) => {
   picture.addEventListener('click', () => {
-    bigPicture.classList.remove('hidden');
-    document.body.classList.add('modal-open');
+    showBigPicture();
+
+    const pictureImg = picture.querySelector('.picture__img').src;
+    const  pictureLikes = picture.querySelector('.picture__likes').textContent;
+    const pictureComments = picture.querySelector('.picture__comments').textContent;
+
+    bigPicture.querySelector('.big-picture__img img').src = pictureImg;
+    bigPicture.querySelector('.likes-count').textContent = pictureLikes;
+    bigPicture.querySelector('.comments-count').textContent = pictureComments;
   });
 });
 
